75924acd2b111b012878b9c4dfeeee4781146dfd
Douglas <douglas.souzag@hotmail.com>
2024-06-02 11:41:38 -0300

    fix: overlaping HUDs when player presses tab

--- src/client/java/com/clockmodeui/ClockModUIClient.java
+++ src/client/java/com/clockmodeui/ClockModUIClient.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 
 public class ClockModUIClient implements ClientModInitializer {
+
   private final ClockUI clockUI = new ClockUI();
   private final CompassUI compassUI = new CompassUI();
 
@@ -15,8 +16,15 @@ public class ClockModUIClient implements ClientModInitializer {
     MinecraftClient client = MinecraftClient.getInstance();
 
     HudRenderCallback.EVENT.register((context, tickDelta) -> {
+      if (isClientPressingTab(client)) {
+        return;
+      }
       clockUI.drawInPlayerUI(context, client);
       compassUI.drawInPlayerUI(context, client);
     });
+  }
+
+  private boolean isClientPressingTab(MinecraftClient client) {
+    return client.options.playerListKey.isPressed();
   }
 }
